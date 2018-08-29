7c24709c5f85b07bfccd01f7d8b4ece61cf9685a
Darren <33794339+Darren2017@users.noreply.github.com>
2018-08-29 18:32:21 +0800

    fix: confilct

--- work_muxixyz_app/decorator.py
+++ work_muxixyz_app/decorator.py
@@ -27,6 +27,5 @@ def login_required(role):
                 abort(401)
             rv=f(uid,*args,**kwargs)
             return rv
-#        middle.__name__=f.__name__
         return decorated_function
     return deco
