5be502e1033c23455022085c58c516035b463bf8
S?O(CCNU) <31346916+ShiinaOrez@users.noreply.github.com>
2018-09-06 16:55:09 +0900

    fixed: export env in /etc/profile

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 DIALECT = 'mysql'
 DRIVER = 'pymysql'
-USERNAME = 'muxitest'
-PASSWORD = 'Muxitest304'
-HOST = 'rm-wz907gsr637s950hmjo.mysql.rds.aliyuncs.com'
-PORT = '3306'
-DATABASE = 'workbenchtest'
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = 3306
+DATABASE = os.getenv("DBNAME")
 
 class Config:
     SECRET_KEY = 'work.muxixyz'
@@ -70,62 +70,3 @@ config = {
     'production': ProductionConfig,
     'default': DevelopmentConfig
 }  
-
-"""'''config for darren'''
-import os
-
-DIALECT = 'mysql'
-DRIVER = 'pymysql'
-USERNAME = 'root'  # os.environ.get('MYSQLUSER')
-PASSWORD = 'root'  # os.environ.get('MYSQLPASSWORD')
-HOST = '127.0.0.1'  # os.environ.get('MYSQLHOST')
-PORT = '3306'
-DATABASE = 'project'
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard ot guess string'
-    SESSION_TYPE = 'filesystem'
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_RECORD_QUERIES = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = \
-        "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,
-                                                     DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                     DATABASE)
-
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = \
-        "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,
-                                                     DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                     DATABASE)
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = \
-        "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,
-                                                     DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                     DATABASE)
-
-    @classmethod
-    def init_app(cls, app):
-        Config.init_app(app)
-
-
-config = {
-    'developments': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
-"""
