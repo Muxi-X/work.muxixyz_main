6b9a35a89aebfa3a1ebadc4e9ee09d8b1c7b1360
ShiinaOrez <shiina_orez@qq.com>
2018-09-16 17:23:58 +0800

    fixed: mq.py 's ENV to getenv

--- work_muxixyz_app/mq.py
+++ work_muxixyz_app/mq.py
@@ -4,9 +4,9 @@ import os
 from work_muxixyz_app import db
 from work_muxixyz_app.models import User
 
-MQHOST = os.getenv("MQHOST") or "120.78.194.125"
-MQUSERNAME = os.getenv("MQUSERNAME") or "feed"
-MQPASSWORD = os.getenv("MQPASSWORD") or "muxixyz"
+MQHOST = os.getenv("WORKBENCH_MQHOST")
+MQUSERNAME = os.getenv("WORKBENCH_MQUSERNAME")
+MQPASSWORD = os.getenv("WORKBENCH_MQPASSWORD")
 
 def newfeed(uid, action, kind, sourceID):
     credentials = pika.PlainCredentials(MQUSERNAME, MQPASSWORD)
