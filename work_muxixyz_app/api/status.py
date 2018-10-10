66b9dd43ca86be23a3ad32c3c3d8bf8ec1591efc
CGH233 <1113713599@qq.com>
2018-10-11 00:12:09 +0800

    fix: fix like bug

--- work_muxixyz_app/api/status.py
+++ work_muxixyz_app/api/status.py
@@ -61,6 +61,7 @@ def getstatu(uid,sid):
     if statu.like is not 0:
         likelen = redis_statu.llen(statu.id)
         likeList = redis_statu.lrange(statu.id,0,likelen)
+        print (likeList)
         if uid in likeList:
             iflike = 1
     user =  User.query.filter_by(id=uid).first()
